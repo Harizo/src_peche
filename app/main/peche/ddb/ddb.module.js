37111ccb2470dc29ac2e51f560442f7005c7cf5f
rltbruce <brucetarafina@gmail.com>
2019-01-25 11:41:29 +0100

    Modification en inserant pagination pays

--- app/main/peche/ddb/ddb.module.js
+++ app/main/peche/ddb/ddb.module.js
@@ -8,10 +8,7 @@
             'app.peche.ddb.localisation', 
             'app.peche.ddb.type_engin',
             'app.peche.ddb.data_collect',
-
             'app.peche.ddb.enqueteur',
-
-            'app.peche.ddb.type_canoe'
 
             ])
         .run(testPermission)
