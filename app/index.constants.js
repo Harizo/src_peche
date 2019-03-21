98fe43d6169543a99fdca24c6b3f2aa9e8790c77
Harizo <rajaonarisoazo@gmail.com>
2019-03-21 17:14:53 +0300

    packet

--- app/index.constants.js
+++ app/index.constants.js
@@ -4,7 +4,23 @@
 
     angular
         .module("fuse")
-		.constant("apiUrl", "http://localhost/2019/peche/api/index.php/api/")
+
+    //Localhost
+		/*.constant("apiUrl", "http://localhost/2019/peche/api/index.php/api/")
 		.constant("apiUrlbase", "http://localhost/2019/peche/api/")
-		.constant("apiUrlserver", "http://localhost/assets/ddb/");
+		.constant("apiUrlserver", "http://localhost/assets/ddb/");*/
+	//Localhost
+
+	//interne ASTRUM
+		.constant("apiUrl", "http://192.168.88.200/2019/peche/api/index.php/api/")
+		.constant("apiUrlbase", "http://192.168.88.200/2019/peche/api/")
+		.constant("apiUrlserver", "http://192.168.88.200/assets/ddb/");
+	//interne ASTRUM
+
+	//externe ASTRUM
+		/*.constant("apiUrl", "http://196.192.38.40/2019/peche/api/index.php/api/")
+		.constant("apiUrlbase", "http://196.192.38.40/2019/peche/api/")
+		.constant("apiUrlserver", "http://196.192.38.40/assets/ddb/");*/
+
+	//externe ASTRUM
 })();
