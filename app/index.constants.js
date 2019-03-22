d0847c180fa1bc94c75d3809aa85c5a6feb316eb
Harizo <rajaonarisoazo@gmail.com>
2019-03-22 10:28:06 +0300

    pack

--- app/index.constants.js
+++ app/index.constants.js
@@ -12,15 +12,15 @@
 	//Localhost
 
 	//interne ASTRUM
-		.constant("apiUrl", "http://192.168.88.200/2019/peche/api/index.php/api/")
+		/*.constant("apiUrl", "http://192.168.88.200/2019/peche/api/index.php/api/")
 		.constant("apiUrlbase", "http://192.168.88.200/2019/peche/api/")
-		.constant("apiUrlserver", "http://192.168.88.200/assets/ddb/");
+		.constant("apiUrlserver", "http://192.168.88.200/assets/ddb/");*/
 	//interne ASTRUM
 
 	//externe ASTRUM
-		/*.constant("apiUrl", "http://196.192.38.40/2019/peche/api/index.php/api/")
+		.constant("apiUrl", "http://196.192.38.40/2019/peche/api/index.php/api/")
 		.constant("apiUrlbase", "http://196.192.38.40/2019/peche/api/")
-		.constant("apiUrlserver", "http://196.192.38.40/assets/ddb/");*/
+		.constant("apiUrlserver", "http://196.192.38.40/assets/ddb/");
 
 	//externe ASTRUM
 })();
