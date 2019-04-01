c2010094f09539acc0c9e0b29efafec622c78c7b
ndrianainaaimebruno <ndrianaina.aime.bruno@gmail.com>
2019-04-01 14:57:24 +0300

    sdfsdfsdfsf

--- app/main/peche/peche.module.js
+++ app/main/peche/peche.module.js
@@ -15,7 +15,7 @@
             'app.peche.ddb',
             'app.peche.reporting',
             'app.peche.importationbdd',
-            'app.peche.valide_fiche_echantillonnage_capture',
+            'app.peche.valide_fiche_echantillonnage_capture'
 
 
         ])
